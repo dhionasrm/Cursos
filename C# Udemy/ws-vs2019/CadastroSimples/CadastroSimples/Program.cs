a031a1271ec98167d0c26edb3e2a318740d67545
dhionas magalhães <dhionas17@hotmail.com>
2021-06-29 09:19:08 -0300

    Código de cadastro simples.

---
+++ C# Udemy/ws-vs2019/CadastroSimples/CadastroSimples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace CadastroSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo: ");
+            string nomeCompleto = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa? ");
+            int quantidadeDeQuartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.WriteLine("Entre com seu último sobrenome, idade e altura");
+            string[ ] vet = Console.ReadLine().Split(' ');
+            string sobrenome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(quantidadeDeQuartos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(sobrenome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
